e0f21294cc3af52ee2518c5be98cb1c28b6d2a43
Javed Ali <30997178+MJavedAli@users.noreply.github.com>
2021-10-14 10:45:32 +0530

    Create ArrayManipulation.java

---
+++ src/arrayProblems/ArrayManipulation.java
@@ -0,0 +1,92 @@
+/*
+Starting with a 1-indexed array of zeros and a list of operations, for each operation add a value to each of the array element between two given indices, inclusive. Once all operations have been performed, return the maximum value in your array.
+
+For example, the length of your array of zeros . Your list of queries is as follows:
+
+a b k
+1 5 3
+4 8 7
+6 9 1
+Add the values of between the indices and inclusive:
+
+index -> 1 2 3  4  5 6 7 8 9 10
+        [0,0,0, 0, 0,0,0,0,0, 0]
+        [3,3,3, 3, 3,0,0,0,0, 0]
+        [3,3,3,10,10,7,7,7,0, 0]
+        [3,3,3,10,10,8,8,8,1, 0]
+The largest value is after all operations are performed.
+  
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static long arrayManipulation(int n, List<List<Integer>> nl) {
+
+    long[] arr = new long[n];
+
+     for(int i=0;i<nl.size();i++) {
+              for(int j=nl.get(i).get(0);j<=nl.get(i).get(1);j++) {
+                  arr[j-1]=arr[j-1]+nl.get(i).get(2);
+              } 
+          }
+      long max = Integer.MIN_VALUE;
+          
+          for(int k=0;k<n;k++) {
+              if(max<arr[k]) {
+                  max = arr[k];
+                  arr[k]=max;
+              }
+          }
+
+        return max;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        IntStream.range(0, m).forEach(i -> {
+            try {
+                queries.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        long result = Result.arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
